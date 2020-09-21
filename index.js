2da21757f13ffd0cca2de0e575b9cef95014fd73
NEvana <54038645+NEvana0003@users.noreply.github.com>
2020-09-21 02:15:33 -0400

    fix command descriptions

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { Plugin } = require('powercord/entities');
-
 module.exports = class b64 extends Plugin {
   startPlugin () {
     powercord.api.commands.registerCommand({
       command: 'encode',
-      description: 'name',
-      usage: '{c} usage',
+      description: 'Base64 Encoder',
+      usage: '{c} <ASCII String>',
       executor: (args) => ({
         send: true,
         result: btoa(args)
@@ -13,8 +12,8 @@ module.exports = class b64 extends Plugin {
     });
     powercord.api.commands.registerCommand({
         command: 'decode',
-        description: 'name',
-        usage: '{c} usage',
+        description: 'Base64 Decoder',
+        usage: '{c} <Base64 String>',
         executor: (args) => ({
           send: true,
           result: atob(args).replace(/,/g,' ')
